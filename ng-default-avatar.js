3d031b7a73029be68cf6a1b6e6c4e794e9d470fe
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2017-06-27 00:06:23 +0200

    Improve files

---
+++ ng-default-avatar.js
@@ -0,0 +1,40 @@
+/*
+ * ng-default-avatar - AngularJS directive to set a default avatar with initials if user has not avatar image
+ * Author: Aitor Rodríguez - http://www.frontendfactory.es/
+ * Version: 0.1
+ * License: MIT
+ */
+
+'use strict';
+
+angular
+	.module('ngDefaultAvatar', [])
+  .controller('defaultAvatarController', ['$scope'], function(scope) {})
+	.directive('defaultAvatar', [
+		function() {
+			return {
+				restrict: 'AE',
+				scope: {
+					name: '=',
+					surname: '=',
+					image: '='
+				},
+				template: '<div class="default-avatar" ng-switch="hasImage"><div ng-switch-when="false">{{ initials.toUpperCase() }}</div><img ng-switch-when="true" ng-src="{{image}}"></div>',
+				link: function(scope, element, attr) {
+					scope.initials = '';
+					scope.hasImage = false;
+
+					if (scope.image !== undefined) {
+						scope.hasImage = true;
+					} else {
+						if (scope.name.length) {
+							scope.initials = scope.name.charAt(0);
+						}
+						if (scope.surname.length) {
+							scope.initials += scope.surname.charAt(0);
+						}
+					}
+				}
+			};
+		}
+	]);
